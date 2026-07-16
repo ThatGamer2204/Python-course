4278bb25e95ae49c8d1fc089a56498da9874fc44
ThatGamer2204 <saketlimaye22@gmail.com>
2026-07-16 13:11:46 +0900

    activitycommit

---
+++ Lesson4/activityyeild.py
@@ -0,0 +1,24 @@
+field1=120
+field2=85
+field3=150
+field4=95
+field5=110
+
+totalyield=field1+field2+field3+field4+field5
+print("Total yield :",totalyield)
+average=(totalyield)/5
+print("Average :",average)
+total_earnings=totalyield*15
+print("Total earnings :",total_earnings)
+fullkgbag=totalyield//25
+print("full kg bags :",fullkgbag)
+leftover=totalyield%25
+print("leftover :",leftover)
+lastyr=500
+print("Compare with last year :",totalyield>lastyr)
+totalyield+=30
+print("Bonus crop :",totalyield)
+totalyield-=15
+print("After removing for seeds :",totalyield)
+fullkgbag=totalyield//25
+print("Recalculated bags :",fullkgbag)
---
+++ lesson3/Agent.py
@@ -0,0 +1,13 @@
+name=input("Enter your name :")
+gadget=input("enter your favourate gadget :")
+agentnumber=7
+speedrating=29.56
+is_Active=True
+is_Active=str(is_Active)
+print(name,type(name))
+print(gadget,type(gadget))
+print(agentnumber,type(agentnumber))
+print(speedrating,type(speedrating))
+print(is_Active,type(is_Active))
+
+print(name[-2:-5:-1])
