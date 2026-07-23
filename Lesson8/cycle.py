153baae0b0de0cdcd68650d220894a1ed920a400
ThatGamer2204 <saketlimaye22@gmail.com>
2026-07-23 21:08:01 +0900

    commit1

---
+++ Lesson8/cycle.py
@@ -0,0 +1,11 @@
+c1=10
+c2=20
+c3=30
+avg=(c1+c2+c3)/3
+print("The average is :",avg)
+if c1<avg :
+    print("First cyclist is slower than average.")
+if c2<avg:
+    print("Second cyclist is slower than average.")
+if c3<avg:
+    print("Third cyclist is slower than average")
---
+++ Lesson8/meancalc.py
@@ -0,0 +1,9 @@
+mean1=38
+wrong_no=36
+correct_no=56
+total_no=40
+sm=mean1*total_no
+sm-=wrong_no
+sm+=correct_no
+mean1=sm/total_no
+print("The correct mean is :",mean1)
