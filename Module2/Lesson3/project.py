e456b424cdb809933c9fa18945247f7f526e7ab3
ThatGamer2204 <saketlimaye22@gmail.com>
2026-08-01 20:21:53 +0900

    commit5

--- Module2/Lesson3/project.py
+++ Module2/Lesson3/project.py
@@ -1,21 +1,21 @@
-        total_homework=4
-        homework_completed=0
-        homework_num=1
-        hw=""
+total_homework=4
+homework_completed=0
+homework_num=1
+hw=""
 
-        while homework_num<=total_homework:
-            if homework_num==1:
-                hw="Make your bed"
-            elif homework_num==2:
-                hw="Feed the pets"
-            elif homework_num==3:
-                hw="Water the plants"        
-            else:
-                hw="Buy groceries"
-            homework_completed=input(f"Have you done the task {task}?").strip().lower()
-            if homework_completed=="yes":
-                homework_completed+=1
-                homework_num+=1
-            else:
-                print(f"Do the homework {hw}!")
-        print("You have sucessfully finished all 4 homeworks !😊")
+while homework_num<=total_homework:
+    if homework_num==1:
+        hw="Math worksheet"
+    elif homework_num==2:
+        hw="physics exercise"
+    elif homework_num==3:
+        hw="Chemistry report"        
+    else:
+        hw="English project"
+    hw_completed=input(f"Have you done the task {hw}?").strip().lower()
+    if hw_completed=="yes":
+        homework_completed+=1
+        homework_num+=1
+    else:
+        print(f"Do the homework {hw}!")
+print("You have sucessfully finished all 4 homeworks !😊")
