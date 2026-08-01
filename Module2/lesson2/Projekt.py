10918208aa6e450610c96c81e08a12acd7bf3984
ThatGamer2204 <saketlimaye22@gmail.com>
2026-08-01 20:04:17 +0900

    commit3

---
+++ Module2/lesson2/Projekt.py
@@ -0,0 +1,4 @@
+num=float(input("Enter the number you want to calculate the power of :"))
+n=float(input("Enter the power you want to calculate :"))
+power=num**n
+print(f"{num} to the power of {n} is {power}.")
