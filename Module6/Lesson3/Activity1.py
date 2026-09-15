dc005e48bd0a3b04fdb2155aa281a74b82489333
ThatGamer2204 <saketlimaye22@gmail.com>
2026-09-15 21:41:07 +0900

    hw

--- Module6/Lesson3/Activity1.py
+++ Module6/Lesson3/Activity1.py
@@ -18,33 +18,75 @@ class Sprite(pygame.sprite.Sprite):
         self.image=pygame.Surface([width,height])
         self.image.fill(color)
         self.rect=self.image.get_rect()
-        self.velocity=[random.choice(-1,1),random.choice(-1,1)]
+        self.velocity=[random.choice([-1,1]),random.choice([-1,1])]
 
     def update(self):
+        # Method to update the sprite's position
+
+        # Move the sprite by its velocity
+
         self.rect.move_ip(self.velocity)
-        edgehit=False
-        if self.rect.left<=0 or self.rect.right>=500:
-            self.velocity*=(-1)
-            edgehit=True
-        if self.rect.top<=0 or self.rect.bottom>=500:
-            self.velocity*=(-1)
-            edgehit=True
-        if edgehit==True:
+
+        # Flag to track if the sprite hits a boundary
+
+        boundary_hit = False
+
+        # Check for collision with left or right boundaries and reverse direction
+
+        if self.rect.left <= 0 or self.rect.right >= 500:
+
+            self.velocity[0] = -1 * self.velocity[0]
+
+            boundary_hit = True
+
+
+        # Check for collision with top or bottom boundaries and reverse direction
+
+        if self.rect.top <= 0 or self.rect.bottom >= 500:
+
+            self.velocity[1] = -self.velocity[1]
+
+            boundary_hit = True
+        if boundary_hit==True:
             pygame.event.post(pygame.event.Event(background_color_change))
             pygame.event.post(pygame.event.Event(sprite_color_change))
+
 
     def colorchange(self):
         self.image.fill(random.choice([blu,wht,org,red]))
 
-clr=0
+clr=blu
 def bgcolor():
     clr=random.choice([ylo,grn,brn])
 
+all_sprites_list=pygame.sprite.Group()
+sp1=Sprite(blu,20,30)
+sp1.rect.x=random.randint(0,480)
+sp1.rect.y=random.randint(0,470)
 
-pygame.display.set_mode((500,500))
+all_sprites_list.add(sp1)
+
+screen=pygame.display.set_mode((500,500))
+cap=pygame.display.set_caption("OOGA BOOGA RULE")
+screen.fill(org)
 done=False
+clock=pygame.time.Clock()
 while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
-        pygame.display.flip()
+        elif event.type==background_color_change:
+            bgcolor()
+        elif event.type==sprite_color_change:
+            sp1.colorchange()
+
+    all_sprites_list.update()
+    screen.fill(clr)
+    all_sprites_list.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(100)
+
+
+
+pygame.quit()
